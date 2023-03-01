70477f33f0b88b9b41d6f5f2327d899d60f57cac
AleixBusquets <33722006+relfecte@users.noreply.github.com>
2023-03-01 19:38:44 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/grupo_11/MainActivity.java
+++ app/src/main/java/com/example/grupo_11/MainActivity.java
@@ -12,6 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+         //Crear intent para la activity a la que llamaremos
+        Intent intent = new Intent(MainActivity.this, AvtivityBusqueda.class);
+
+
+        // Crear un objeto handler y usar el etodo postDelayed para retrasar la abertura de la ActivityBusqueda por 3 segundos
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        }, 3000);
 
         
 
