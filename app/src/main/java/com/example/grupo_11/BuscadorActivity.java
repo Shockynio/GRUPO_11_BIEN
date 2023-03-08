1ffc61713f92fd2b15bdcf7e87fece58b5e9197d
Shockynio <74144399+Shockynio@users.noreply.github.com>
2023-03-08 15:34:09 +0100

    final proyect

--- app/src/main/java/com/example/grupo_11/BuscadorActivity.java
+++ app/src/main/java/com/example/grupo_11/BuscadorActivity.java
@@ -3,6 +3,12 @@ package com.example.grupo_11;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.widget.Toast;
+import android.widget.Button;
+import android.view.View;
+
+
+
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,19 +29,22 @@ public class BuscadorActivity extends AppCompatActivity implements SearchView.On
     RecyclerView listaEmpleados;
     ArrayList <Empleados> listaArrayEmpleados;
     ListaEmpleadosAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscador);
-        listaEmpleados= findViewById(R.id.listaEmpleados);
-        txtBuscar= findViewById(R.id.txtBuscar);
+
+        listaEmpleados = findViewById(R.id.listaEmpleados);
+        txtBuscar = findViewById(R.id.txtBuscar);
         listaEmpleados.setLayoutManager(new LinearLayoutManager(this));
 
-        DbEmpleados dbEmpleados = new DbEmpleados((BuscadorActivity.this));
+        DbEmpleados dbEmpleados = new DbEmpleados(BuscadorActivity.this);
 
         listaArrayEmpleados = new ArrayList<>();
+        listaArrayEmpleados.addAll(dbEmpleados.mostrarEmpleados());
 
-        adapter = new ListaEmpleadosAdapter(dbEmpleados.mostrarEmpleados());
+        adapter = new ListaEmpleadosAdapter(listaArrayEmpleados);
         listaEmpleados.setAdapter(adapter);
 
         txtBuscar.setOnQueryTextListener(this);
@@ -53,11 +62,14 @@ public class BuscadorActivity extends AppCompatActivity implements SearchView.On
             case R.id.menuNuevo:
                 nuevoRegistro();
                 return true;
-
+            case R.id.menuActualizar:
+                actualizarDatos();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+
 
     private void nuevoRegistro(){
         Intent intent = new Intent(this, NuevoActivity.class);
@@ -75,4 +87,20 @@ public class BuscadorActivity extends AppCompatActivity implements SearchView.On
         adapter.filtrado(s);
         return false;
     }
+
+    private void actualizarDatos(){
+        DbEmpleados dbEmpleados = new DbEmpleados(this);
+        listaArrayEmpleados.clear();
+        listaArrayEmpleados.addAll(dbEmpleados.mostrarEmpleados());
+        adapter.notifyDataSetChanged();
+
+        Toast.makeText(this, "Datos actualizados", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+
 }
+
+
